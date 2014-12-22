ce8109c09f74b361378a2a04cb62f7525d109749
Amr <eldib.amr@gmail.com>
2014-12-21 20:35:33 -0800

    Schema, UI, and Initial data
    - Created JSON schema.
    - Created UI, and modified background image.
    - Added some initial data.
    - Used compiled handlebars templates.

---
+++ Scripts/awesomeGis.js
@@ -0,0 +1,31 @@
+﻿/// <reference path="jquery-2.1.1.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="handlebars.runtime.js" />
+/// <reference path="../templates/navTemplate.hbs.js" />
+/// <reference path="../templates/samplesTemplate.hbs.js" />
+
+try {
+    $.get('data.json', function (samplesData) {
+        $('#samplesContainer').html(samplesTemplate(samplesData));
+
+        $(function () {
+            $('#txtFilter').fastLiveFilter('.samplesList', {
+                timeout: 200,
+                callback: function (total) {
+                    console.log("Found: " + total);
+                }
+            });
+        });
+        $('#navContainer').html(navTemplate(samplesData));
+
+        $(window).scroll(function () {
+            if ($(document).scrollTop() > 50) {
+                $('#banner').addClass('shrink');
+            } else {
+                $('#banner').removeClass('shrink');
+            }
+        });
+    });
+} catch (e) {
+    console.log(e);
+}
